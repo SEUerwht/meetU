7afa704003bc9f5c9838898aec76c6bdd06577c9
SEUerwht <2576196737@qq.com>
2022-10-13 14:31:22 +0800

    修改用户表

--- util/config.py
+++ util/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@localhost:3306/test"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@localhost:3306/meetu"
 NOT_CHECK_URL = ["/user/login",'/','/user/register']
 SECURITY_KEY = "wwanghaitaoqqxqzgrb"
 REDIS_HOST = "127.0.0.1"
