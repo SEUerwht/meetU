9638d13b08c11364b6843b2984678d113fdf763d
SEUerwht <2576196737@qq.com>
2022-10-13 14:54:07 +0800

    修改拦截器

--- model/User.py
+++ model/User.py
@@ -8,3 +8,4 @@ class user(BaseModel):
     phone = db.Column(db.String(20), nullable=True, comment="电话号码", unique=True)
     email = db.Column(db.String(30), nullable=True, comment="邮箱", unique=True)
     age = db.Column(db.Integer, nullable=True, comment="年龄")
+    gender = db.Column(db.Integer, nullable=True,comment="性别")
