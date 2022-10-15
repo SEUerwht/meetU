0123c344566d63aacea3ad9f5c929b6408250c75
淘淘wht <2576196737@qq.com>
2022-10-15 20:06:21 +0800

    修改group_api

---
+++ service/model2dist.py
@@ -0,0 +1,43 @@
+from flask import make_response, jsonify
+from model.BaseModel import BaseModel
+from datetime import datetime
+
+
+def model_to_dict(model):
+    """模型转字典"""
+    if type(model) is list:
+        return [model_to_dict(e) for e in model]
+    if not model:
+        return None
+
+    d = {}
+    for c in model.__table__.columns:
+        attr = getattr(model, c.name)
+        if isinstance(attr, datetime):
+            # 格式化时间字段
+            attr = attr.strftime("%Y-%m-%d %H:%M:%S")
+        d[c.name] = attr
+    return d
+
+
+def response(data=None, msg="success", status=200):
+    """封装响应"""
+    print(type(data[0]))
+    if isinstance(data[0], BaseModel):
+        # data为单个对象
+        print(1)
+        data_dist = model_to_dict(data)
+        data_dist["username"] = data[1]
+        print(data_dist)
+    elif type(data) is list:
+        if not data:
+            data_dist = []
+        else:
+            data_dist = []
+            for model in data:
+                temp = model_to_dict(model[0])
+                temp["username"] = model[1]
+                data_dist.append(temp)
+    body = jsonify(data=data_dist, msg=msg)
+    response_ = make_response(body, status)
+    return response_
