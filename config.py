95e39ce7708fd9125dc25525e571b4e904951e2c
Duing <2385790938@qq.com>
2022-10-23 21:16:13 +0800

    update

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ NOT_CHECK_URL = [
     '/',
     '/user/register'
 ]
-SECURITY_KEY = "wwanghaitaoqqxqzgrb"
+SECURITY_KEY = "dhAfi656grs126Ud7afFYEFH8aesfwa9ahH"
 REDIS_HOST = "127.0.0.1"
 REDIS_PORT = 6379
 EXPIRE_TIME = 7 * 24 * 60 * 60
-SAVE_FILE_PATH = "/" if IS_LOCOLHOST else "/"
-FILE_LEGAL = ['png','jpg']
-BASE_URL = ""
+SAVE_FILE_PATH = "/" if IS_LOCOLHOST else "/MeetU/savefiles/"
+FILE_LEGAL = ['png', 'jpg']
+BASE_URL = "http://aitmaker.cn:6555/"
 HELP_PATH = "/files/help.docx"
